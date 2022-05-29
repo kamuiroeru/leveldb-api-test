7bc0338d3cf332253391d303b697366ddaf46a73
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2022-05-29 21:50:50 +0900

    initial_commit

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import express from 'express'
+import level from 'level-ts'
+
+import { generateUuid } from './modules/uuid'
+import type { Component, Quotation } from './model/models'
+
+// const db = new level(`${process.cwd()}/leveldb`)
+const app = express()
+app.use(express.json)
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = 8888
+
+// CROS対応（というか完全無防備：本番環境ではだめ絶対）
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+})
+
+app.get('/', (req, res) => {
+  // res.status(200).json({
+  res.json({
+    status: 'ok'
+  })
+})
+
+// app.post('/gen-mock', async (req, res) => {
+//   const id = generateUuid();
+//   const requestQuotation: Quotation = {
+//     id,
+//     cpu: {
+//       id: generateUuid(),
+//       name: 'Intel Core i9-12900K',
+//       url: 'https://www.dospara.co.jp/5shopping/detail_parts.php?bg=1&br=11&sbr=1329&ic=475986&utm_source=kakaku.com&utm_medium=referral&utm_campaign=kakaku_parts_camp&_bdadid=JPGTE5.00002isv&lf=0',
+//       price: 75980,
+//       shop: 'ドスパラ',
+//       watts: null
+//     },
+//     totalPrice: 75980,
+//     totalWatts: 150,
+//     text: '',
+//   }
+
+//   db.put(id, requestQuotation)
+//   res.status(200)
+// })
+
+// app.post('/pc', async (req, res) => {
+//   const requestQuotation: Quotation = req.body
+//   const uuid = generateUuid()
+//   requestQuotation.id = uuid;
+//   await db.put(uuid, requestQuotation)
+// })
+
+// app.get('/pc/:id', async (req, res) => {
+//   const id = req.params.id
+//   const requestQuotation: Quotation = await db.get(id)
+//   console.log({ requestQuotation })
+//   res.status(200).send(requestQuotation)
+// })
+
+app.listen(PORT, () => {
+  console.log(`Start on port ${PORT}.`)
+})
---
+++ src/model/models.ts
@@ -0,0 +1,27 @@
+export type Component = {
+  id: string
+  name: string
+  url: string
+  price: number
+  shop: string
+  watts: null | number
+}
+
+export type Quotation = {
+  id: string
+  cpu?: Component
+  cpuCooler?: Component
+  memory?: Component
+  motherBoard?: Component
+  videoCard?: Component
+  soundCard?: Component
+  ssd?: Component
+  hdd?: Component
+  diskDrive?: Component
+  case?: Component
+  powerSupply?: Component
+  os?: Component
+  totalPrice: number
+  totalWatts: number
+  text: string
+}

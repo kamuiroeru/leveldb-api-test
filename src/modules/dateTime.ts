72e76f0ff967c8b904ed88beb93cef6364c8352b
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2022-06-06 00:51:51 +0900

    [update] clean

--- src/modules/dateTime.ts
+++ src/modules/dateTime.ts
@@ -1,3 +1,7 @@
+/**
+ * 現在日時の ISO 文字列 (UTC) を返す
+ * @returns ISO文字列
+ */
 export const nowIsoString = (): string => {
   return new Date().toISOString()
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,21 @@
 import supertest from 'supertest'
 import app from '../src/index'
+import * as myUuid from '../src/modules/uuid'
 
 describe('サーバのテスト', () => {
   it('Path [/] のテスト', async () => {
     const res = await supertest(app).get('/')
     expect(res.statusCode).toBe(200)
     expect(res.body.status).toBe('ok')
+  })
+
+  it('Path [/uuid] のテスト', async () => {
+    //
+    const spy = jest.spyOn(myUuid, 'generateUuid').mockReturnValue('generatedUUID')
+    // const spy = jest.spyOn(dateTime, 'nowIsoString').mockReturnValue('2022-06-05T12:34:56.789Z')
+    const res = await supertest(app).get('/uuid')
+    expect(res.statusCode).toBe(200)
+    expect(res.body.v4).toBe('generatedUUID')
+    expect(spy).toBeCalledTimes(1)
   })
 })

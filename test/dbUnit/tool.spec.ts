e1974562f28833dd511ee6449ec48e00409071bb
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2022-06-08 23:30:02 +0900

    [add] 単体テスト追加 #4

---
+++ test/dbUnit/tool.spec.ts
@@ -0,0 +1,21 @@
+import Level from 'level-ts'
+import FsPromises from 'node:fs/promises'
+import * as tool from './tool'
+
+jest.mock('Level') // モジュールルートで実施する必要がある
+
+describe('tool.tsのテスト', () => {
+  it('dumpDbToJson のテスト', async () => {
+    // これだけ coverage に寄与したので書く
+    // モックの設定
+    const db = new Level('')
+    const mockDbAll = jest.spyOn(db, 'all').mockImplementation(async () => [])
+    const mockWriteFile = jest.spyOn(FsPromises, 'writeFile').mockImplementation(async (outPath, db) => { })
+    // 実行
+    const outPath = '/path/to/output.json'
+    await tool.dumpDbToJson(outPath, db)
+    // チェック
+    expect(mockDbAll).toBeCalledTimes(1)
+    expect(mockWriteFile).toBeCalledWith(outPath, '[]')
+  })
+})
---
+++ test/modules/uuid.spec.ts
@@ -0,0 +1,12 @@
+import { generateUuid } from '../../src/modules/uuid'
+
+const isUuidV4 = (s: string): boolean => {
+  return /^[0-f]{8}-[0-f]{4}-4[0-f]{3}-[8-f][0-f]{3}-[0-f]{12}$/.test(s)
+}
+
+describe('uuidモジュールのテスト', () => {
+  const args = [...Array(10).keys()].map(i => (' ' + (i + 1)).slice(-2)) // 2桁に揃えた引数
+  test.each(args)('generateUuidのテスト %s 回目', (_s: string) => {
+    expect(isUuidV4(generateUuid())).toBe(true)
+  })
+})

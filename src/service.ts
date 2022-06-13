cde6a63d98cb6ef67b551606913d27cfc4d32b79
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-06-13 21:52:30 +0900

    [bugfix] while の条件 と テストが間違っていたので修正

--- src/service.ts
+++ src/service.ts
@@ -30,7 +30,8 @@ export class PcServiceImpl implements PcService {
     let uuid = ''
     do {
       uuid = generateUuid()
-    } while (!(await this.repository.exists(uuid)))
+      console.log({ uuid })
+    } while (await this.repository.exists(uuid))
     const copied: Quotation = JSON.parse(JSON.stringify(quotation))
     copied.id = uuid // id を付与する
     await this.repository.put(uuid, copied)
